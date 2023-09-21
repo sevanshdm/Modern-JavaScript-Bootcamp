50944bd1099141897ae11f865104608f3ff403f4
sevans <sevans@highdesertmilk.com>
2023-09-21 15:47:19 -0600

    Video 42: Searching Arrays: Part 1

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,21 +1,26 @@
-
-
-const notes = ['Note 1', 'Note 2', 'Note 3']
+const notes = [{}, {
+    title: 'My next trip',
+    body: 'I would like to go to Brazil'
+},{
+    title: 'Habits to work on',
+    body: 'Exercise, Eating a bit better'
+},{
+    Title: 'Office modification',
+    body: 'Get a new seat'
+},]
+/*
 notes[2] = 'This is the new note 3'
 
 notes.forEach(function (item, index){
     console.log(index)
     console.log(item)
 })
-
+*/
 console.log(notes.length)
 console.log(notes)
 
-//Counting... 1
-for (let count = 0; count <=2; count ++) {
-    console.log(count)
-}
-
-for(let count = notes.length - 1; count >=0 ; count--){
-    console.log(notes[count])
-}
+const index = notes.findIndex(function(note, index){
+    console.log(note)
+    return note.title === 'Habits to work on'
+})
+console.log(index)
