b78be7cc1230a01d44406b6817dbb341570dccee
sevans <sevans@highdesertmilk.com>
2023-09-22 15:26:20 -0600

    Video 56: Rendering Our Filtered Data

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -9,6 +9,26 @@ const notes = [{
     body: 'Get a new seat'
 }]
 
+const filters = {
+    searchText: ''
+}
+
+const renderNotes = function(notes, filters){
+    const filteredNotes = notes.filter(function(note){
+        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+
+    document.querySelector('#notes').innerHTML = ''
+    
+    filteredNotes.forEach(function(note){
+        const noteEl = document.createElement('p')
+        noteEl.textContent = note.title
+        document.querySelector('#notes').appendChild(noteEl)
+    })
+}
+
+renderNotes(notes, filters)
+
 document.querySelector('#create-note').addEventListener('click', function(event){
     event.target.textContent = 'The button was clicked'
 })
@@ -20,5 +40,6 @@ document.querySelector('#remove-all').addEventListener('click', function(event){
 })
 
 document.querySelector('#search-text').addEventListener('input', function(event){
-    console.log(event.target.value)
+    filters.searchText = event.target.value
+    renderNotes(notes, filters)
 })
