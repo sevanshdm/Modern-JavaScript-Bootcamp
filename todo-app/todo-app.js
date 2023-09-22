07196415ed0f2f630b4a6b7c94fcbcb733d416e8
sevans <sevans@highdesertmilk.com>
2023-09-22 15:55:58 -0600

    VIdeo 57: Todo Filter Challenge

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -15,28 +15,46 @@ const todos = [{
     completed: true
 }]
 
-const incompleteTodos = todos.filter(function(todo){
-    return !todo.completed
-})
+const filters = {
+    searchText: ''
+}
 
-const summary = document.createElement('h2')
+const renderTodos = function(todos,filters){
+    const filteredTodos = todos.filter(function(todo){
+        return todo.text.toLocaleLowerCase().includes(filters.searchText.toLowerCase())
+    })
+    const incompleteTodos = filteredTodos.filter(function(todo){
+        return !todo.completed
+    })
 
-summary.textContent = `You have ${incompleteTodos.length} todos left`
-
-document.querySelector('body').appendChild(summary)
-
-todos.forEach(function(todo){
-    const p = document.createElement('p')
-    p.textContent = todo.text
-    document.querySelector('body').appendChild(p)
-})
+    document.querySelector('#todos').innerHTML = ''
+    
+    const summary = document.createElement('h2')
+    
+    summary.textContent = `You have ${incompleteTodos.length} todos left`
+    
+    document.querySelector('#todos').appendChild(summary)
+    
+    filteredTodos.forEach(function(todo){
+        const p = document.createElement('p')
+        p.textContent = todo.text
+        document.querySelector('#todos').appendChild(p)
+    })
+}
 
 // Listen for new todo creation
 document.querySelector('#add-todo').addEventListener('click',function(event){
     console.log('Add a new todo...')
 })
 
+renderTodos(todos, filters)
+
 // Listen for todo text change
 document.querySelector('#new-todo-text').addEventListener('input', function(event){
     console.log(event.target.value)
+})
+
+document.querySelector('#search-text').addEventListener('input',function(event){
+    filters.searchText = event.target.value
+    renderTodos(todos, filters)
 })
