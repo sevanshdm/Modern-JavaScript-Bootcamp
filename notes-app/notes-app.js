8c9fa2d5bf30fe95860498eb7137169635409369
sevans <sevans@highdesertmilk.com>
2023-09-22 13:11:42 -0600

    Video 52: Adding Elements via the DOM

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,3 +1,14 @@
+const notes = [{
+    title: 'My next trip',
+    body: 'I would like to go to Brazil'
+}, {
+    title: 'Habits to work on',
+    body: 'Exercise, Eating a bit better'
+}, {
+    title: 'Office modification',
+    body: 'Get a new seat'
+}]
+
 // DOM - Document Object Model
 
 // Query and remove
@@ -12,3 +23,8 @@ ps.forEach(function(p){
     //console.log(p.textContent)
     //p.remove()
 })
+
+// Add a new element
+const newParagraph = document.createElement('p')
+newParagraph.textContent = 'This is a new element from JavaScript'
+document.querySelector('body').appendChild(newParagraph)
--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -1,7 +1,32 @@
-const paragraphs = document.querySelectorAll('p')
+const todos = [{
+    text: 'Go home',
+    completed: true
+}, {
+    text: 'Workout',
+    completed: false
+}, {
+    text: 'Take a shower',
+    completed: true
+}, {
+    text: 'Eat dinner',
+    completed: false
+}, {
+    text: 'Go to bed',
+    completed: true
+}]
 
-paragraphs.forEach(function(paragraph){
-    if(paragraph.textContent.includes('the')){
-        paragraph.remove()
-    }
+const incompleteTodos = todos.filter(function(todo){
+    return !todo.completed
+})
+
+const summary = document.createElement('h2')
+
+summary.textContent = `You have ${incompleteTodos.length} todos left`
+
+document.querySelector('body').appendChild(summary)
+
+todos.forEach(function(todo){
+    const p = document.createElement('p')
+    p.textContent = todo.text
+    document.querySelector('body').appendChild(p)
 })
