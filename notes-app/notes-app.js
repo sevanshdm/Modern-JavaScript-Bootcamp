f1bac72909680409d9003422a489046616b5982e
sevans <sevans@highdesertmilk.com>
2023-09-25 10:47:34 -0600

    Video 62: Saving Data in Local Storage pt 1

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -13,6 +13,19 @@ const filters = {
     searchText: ''
 }
 
+// const user = {
+//     name: 'Shrek',
+//     age: 33
+// }
+
+// const userJSON = JSON.stringify(user)
+// console.log(userJSON)
+// localStorage.setItem('user', userJSON)
+
+const userJSON = localStorage.getItem('user')
+const user = JSON.parse(userJSON)
+console.log(`${user.name} is ${user.age}`)
+
 const renderNotes = function(notes, filters){
     const filteredNotes = notes.filter(function(note){
         return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
