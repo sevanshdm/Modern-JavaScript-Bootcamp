6544a2fb0cf5555fa080ec4a589a347b01f2167c
sevans <sevans@highdesertmilk.com>
2023-09-25 12:00:08 -0600

    Video 64: Splitting up Our Application

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,33 +1,7 @@
-let notes = []
+const notes = getSavedNotes()
 
 const filters = {
     searchText: ''
-}
-//Check for existing saved data
-let notesJSON = localStorage.getItem('notes')
-
-if (notesJSON !== null){
-    notes = JSON.parse(notesJSON)
-}
-
-const renderNotes = function(notes, filters){
-    const filteredNotes = notes.filter(function(note){
-        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
-    })
-
-    document.querySelector('#notes').innerHTML = ''
-    
-    filteredNotes.forEach(function(note){
-        const noteEl = document.createElement('p')
-
-        if (note.title.length > 0){
-            noteEl.textContent = note.title
-        } else{
-            noteEl.textContent = 'Unnamed note'
-        }
-
-        document.querySelector('#notes').appendChild(noteEl)
-    })
 }
 
 renderNotes(notes, filters)
@@ -38,7 +12,8 @@ document.querySelector('#create-note').addEventListener('click', function(event)
         title: '',
         body: ''
     })
-    localStorage.setItem('notes', JSON.stringify(notes))
+
+    saveNotes(notes)
     renderNotes(notes,filters)
 })
 
