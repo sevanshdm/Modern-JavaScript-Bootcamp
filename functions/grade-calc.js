10335b23edfb8a808f5d0ef4dcc6bd4250d79f7b
sevans <sevans@highdesertmilk.com>
2023-09-26 15:43:36 -0600

    Video 85: Catching and Throwing Errors

--- functions/grade-calc.js
+++ functions/grade-calc.js
@@ -1,7 +1,42 @@
 // Students score, total possible score
 // 15/20 -> You got a C (75%)!
 
-const calc = function(score, possibleScore){
+// Before restructuring 
+// const gradeCalc = function(score, possibleScore){
+//     if (typeof score === 'number' && typeof possibleScore === 'number') {
+//         const percent = (score/possibleScore) * 100
+//         let letterGrade =''
+    
+//         if (percent >= 90){
+//             letterGrade = 'A'
+//         } else if(percent >= 80) {
+//             letterGrade = 'B'
+//         } else if(percent >= 70) {
+//             letterGrade = 'C'
+//         } else if(percent >= 60) {
+//             letterGrade = 'D'
+//         } else {
+//             letterGrade = 'F'
+//         }
+//         return `You got a ${letterGrade} (${percent}%)`
+//     } else {
+//         throw Error('Please provide numbers only')
+//     }
+// } 
+
+//  try {
+//     const result = gradeCalc(true, 20)
+//     console.log(result)
+//  } catch(e) {
+//     console.log(e.message) // Displays the error message from Error
+//  }
+
+// After 
+const gradeCalc = function(score, possibleScore){
+    if (typeof score !== 'number' || typeof possibleScore !== 'number') {
+        throw Error('Please provide numbers only')
+    }
+    
     const percent = (score/possibleScore) * 100
     let letterGrade =''
 
@@ -17,7 +52,11 @@ const calc = function(score, possibleScore){
         letterGrade = 'F'
     }
     return `You got a ${letterGrade} (${percent}%)`
-}
+} 
 
-const result = calc(15, 20)
-console.log(result)
+ try {
+    const result = gradeCalc(true, 20)
+    console.log(result)
+ } catch(e) {
+    console.log(e.message) // Displays the error message from Error
+ }
