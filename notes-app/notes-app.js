32709893926c037901cb74dcab2ecef99615dcf8
sevans <sevans@highdesertmilk.com>
2023-09-26 10:16:34 -0600

    Video 74: JavaScript Dates

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -33,3 +33,28 @@ window.addEventListener('storage', function(event){
         renderNotes(notes,filters)
     }
 })
+
+// Unix Epoch - January 1st 1970 00:00:00
+const now = new Date()
+const timestamp = now.getTime()
+
+const myDate = new Date(timestamp)
+console.log(myDate.getFullYear())
+
+const dateOne = new Date('March 1 2017 12:00:00')
+const dateTwo = new Date()
+const dateOneTimestamp = dateOne.getTime()
+const dateTwoTimestamp = dateTwo.getTime()
+
+if (dateOneTimestamp < dateTwoTimestamp) {
+    console.log(dateOne.toString())
+} else if (dateTwoTimestamp < dateOneTimestamp) {
+    console.log(dateTwo.toString())
+}
+
+// console.log(`Year: ${now.getFullYear()}`)
+// console.log(`Month: ${now.getMonth()}`)
+// console.log(`Day: ${now.getDate()}`)
+// console.log(`Hour: ${now.getHours()}`)
+// console.log(`Minute: ${now.getMinutes()}`)
+// console.log(`Seconds: ${now.getSeconds()}`)
