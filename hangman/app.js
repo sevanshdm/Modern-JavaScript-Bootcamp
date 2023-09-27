415146131095043154a8d9f4e5b9d069215488ec
sevans <sevans@highdesertmilk.com>
2023-09-27 15:17:29 -0600

    Video 104: HTTP Requests from JavaScript

--- hangman/app.js
+++ hangman/app.js
@@ -1,3 +1,7 @@
+// HTTP (Hypertext Transfer Protocol)
+// Request - What do we want to do
+// Response - What was actually done
+
 const puzzleEl = document.querySelector('#puzzle')
 const guessesEl = document.querySelector('#guesses')
 
@@ -12,3 +16,17 @@ window.addEventListener('keydown', function(event){
     puzzleEl.textContent = game1.Puzzle
     guessesEl.textContent = game1.StatusMessage
 })
+
+// Making an HTTP request
+const request = new XMLHttpRequest()
+
+request.addEventListener('readystatechange', (event) => {
+    if (event.target.readyState === 4) {
+        const data = JSON.parse(event.target.responseText)
+        console.log(data)
+    }
+})
+
+// GET is the method as shown in the network tab in the dev tools
+request.open('GET', 'https://puzzle.mead.io/puzzle')
+request.send()
