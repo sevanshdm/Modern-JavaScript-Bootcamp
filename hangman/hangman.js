2c29fe56da781d4b74a79648e685ce2658380a0f
sevans <sevans@highdesertmilk.com>
2023-09-27 11:08:05 -0600

    Video 96: Hangman Challenge Part 2

---
+++ hangman/hangman.js
@@ -0,0 +1,45 @@
+//Constructor
+const Hangman = function (word, remainingGuesses) {
+    this.word = word.toLowerCase().split('')
+    this.remainingGuesses = remainingGuesses
+    this.guessedLetters = []
+}
+
+Hangman.prototype.getPuzzle = function(){
+    let puzzle = ''
+    
+    this.word.forEach((letter) => {
+        if (this.guessedLetters.includes(letter) || letter === ' ') {
+            puzzle +=  letter
+        } else {
+            puzzle += '*'
+        }
+    })
+
+    return puzzle
+}
+
+Hangman.prototype.makeGuess = function(guess){
+    guess = guess.toLowerCase()
+    const isUnique = !this.guessedLetters.includes(guess)
+    const isBadGuess = !this.word.includes(guess)
+
+    if(isUnique){
+        this.guessedLetters.push(guess)
+    } 
+
+    if(isUnique && isBadGuess){
+        this.remainingGuesses--
+    }
+}
+
+const game1 = new Hangman('Cat', 2)
+console.log(game1.getPuzzle())
+console.log(game1.remainingGuesses)
+
+window.addEventListener('keydown', function(event){
+    const guess = event.key
+    game1.makeGuess(guess)
+    console.log(game1.getPuzzle())
+    console.log(game1.remainingGuesses)
+})
