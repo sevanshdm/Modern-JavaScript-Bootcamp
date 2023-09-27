ac7070e797ca683d2964a3d8a346979ffec3849e
sevans <sevans@highdesertmilk.com>
2023-09-27 13:04:57 -0600

    Video 99: The Class Syntax

--- hangman/hangman.js
+++ hangman/hangman.js
@@ -1,81 +1,62 @@
 //Constructor
-const Hangman = function (word, remainingGuesses) {
-    this.word = word.toLowerCase().split('')
-    this.remainingGuesses = remainingGuesses
-    this.guessedLetters = []
-    this.status = 'playing'
-}
-
-Hangman.prototype.calculateStatus = function(){
-    const finished = this.word.every((letter) => this.guessedLetters.includes(letter))
-    // Two others ways to make this method below
-
-    // const lettersUnguessed = this.word.filter((letter) =>{
-    //     return !this.guessedLetters.includes(letter)
-    // })
-    // const finished = lettersUnguessed.length === 0
-
-
-    // let finished = true
-
-    // this.word.forEach((letter) => {
-    //     if (this.guessedLetters.includes(letter)){
-
-    //     } else {
-    //         finished = false
-    //     }
-    // })
-
-    if (this.remainingGuesses === 0) {
-        this.status = 'failed'
-    } else if (finished) {
-        this.status = 'finished'
-    } else {
+class Hangman {
+    constructor(word, remainingGuesses) {
+        this.word = word.toLowerCase().split('')
+        this.remainingGuesses = remainingGuesses
+        this.guessedLetters = []
         this.status = 'playing'
     }
-}
-
-Hangman.prototype.getStatusMessage = function(){
-    if (this.status === 'playing') {
-        return `Guesses Left: ${this.remainingGuesses}`
-    } else if (this.status === 'failed') {
-        return `Nice try! The word was "${this.word.join('')}".`
-    } else {
-        return 'Great work! You guessed the word.'
-    }
-}
-
-Hangman.prototype.getPuzzle = function(){
-    let puzzle = ''
+    calculateStatus() {
+        const finished = this.word.every((letter) => this.guessedLetters.includes(letter))
     
-    this.word.forEach((letter) => {
-        if (this.guessedLetters.includes(letter) || letter === ' ') {
-            puzzle +=  letter
+        if (this.remainingGuesses === 0) {
+            this.status = 'failed'
+        } else if (finished) {
+            this.status = 'finished'
         } else {
-            puzzle += '*'
+            this.status = 'playing'
         }
-    })
-
-    return puzzle
-}
-
-Hangman.prototype.makeGuess = function(guess){
-    guess = guess.toLowerCase()
-    const isUnique = !this.guessedLetters.includes(guess)
-    const isBadGuess = !this.word.includes(guess)
-
-    if (this.status !== 'playing') {
-        return
     }
-
-    if(isUnique){
-        this.guessedLetters.push(guess)
-    } 
-
-    if(isUnique && isBadGuess){
-        this.remainingGuesses--
+    getStatusMessage() {
+        if (this.status === 'playing') {
+            return `Guesses Left: ${this.remainingGuesses}`
+        } else if (this.status === 'failed') {
+            return `Nice try! The word was "${this.word.join('')}".`
+        } else {
+            return 'Great work! You guessed the word.'
+        }
     }
-
-
-    this.calculateStatus()
+    getPuzzle() {
+        let puzzle = ''
+    
+        this.word.forEach((letter) => {
+            if (this.guessedLetters.includes(letter) || letter === ' ') {
+                puzzle +=  letter
+            } else {
+                puzzle += '*'
+            }
+        })
+    
+        return puzzle
+    }
+    makeGuess(guess) {
+        guess = guess.toLowerCase()
+        const isUnique = !this.guessedLetters.includes(guess)
+        const isBadGuess = !this.word.includes(guess)
+    
+        if (this.status !== 'playing') {
+            return
+        }
+    
+        if(isUnique){
+            this.guessedLetters.push(guess)
+        } 
+    
+        if(isUnique && isBadGuess){
+            this.remainingGuesses--
+        }
+    
+    
+        this.calculateStatus()
+    }
 }
--- oop/person.js
+++ oop/person.js
@@ -1,29 +1,27 @@
 // Prototypal Inheritance
+class Person {
+    // Constructor function (typically have capital first letter.(Constructors are meant to be called with "new"))
+    // Do not put return in constructor functions.
+    constructor(firstName, lastName, age, likes = []) {
+        this.firstName = firstName
+        this.lastName = lastName
+        this.age = age
+        this.likes = likes
+    }
+    getBio() {
+        let bio = `${this.firstName} is ${this.age}.` // this line is the parent "this." as referred to below on line 16
 
-// Constructor function (typically have capital first letter.(Constructors are meant to be called with "new"))
-// Do not put return in constructor functions.
-const Person = function (firstName, lastName, age, likes = []) {
-    this.firstName = firstName
-    this.lastName = lastName
-    this.age = age
-    this.likes = likes
-}
-
-// prototype object is how to create a method on the constructor function(Person) .getBio is the new method's name
-Person.prototype.getBio = function () {
-    let bio = `${this.firstName} is ${this.age}.` // this line is the parent "this." as referred to below on line 16
-
-    this.likes.forEach((like) => { 
-        bio +=` ${this.firstName} likes ${like}.` // Arrow functions don't bind "this." values, so it uses what ever "this." value its parent has
-    })
-
-    return bio
-}
-
-Person.prototype.setName = function(fullName) {
-    const names = fullName.split(' ')
-    this.firstName = names[0]
-    this.lastName = names[1]
+        this.likes.forEach((like) => { 
+            bio +=` ${this.firstName} likes ${like}.` // Arrow functions don't bind "this." values, so it uses what ever "this." value its parent has
+        })
+    
+        return bio
+    }
+    setName(fullName) {
+        const names = fullName.split(' ')
+        this.firstName = names[0]
+        this.lastName = names[1]
+    }
 }
 
 const me = new Person('Shrek', 'Swampington', 33, ['Teaching', 'Biking'])
