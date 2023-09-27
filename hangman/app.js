e4ea424e43d480f0a8ba65dfa9cc669f9f946bc8
sevans <sevans@highdesertmilk.com>
2023-09-27 10:40:47 -0600

    Video 95: Primitive and Objects Part 2

--- hangman/app.js
+++ hangman/app.js
@@ -1,8 +1,15 @@
-// product --> Object.prototype --> null
-const product = new Object({
-    name: 'The War Of Art'
-})
+// Primitive value: string, number, boolean, null, undefined
 
-// hasOwnProperty
-console.log(product.hasOwnProperty('price'))
+// Prototype chains
+// Object: myObject --> Object.prototype --> null
+// Array: myArray --> Array.prototype --> Object.prototype --> null
+// Function: myFunc --> Function.prototype --> Object.prototype --> null
+// String: myString --> String.prototype --> Object.prototype --> null
+// Number: myNumber --> Number.prototype --> Object.prototype --> null
+// Boolean: myBoolean --> Boolean.prototype --> Object.prototype --> null
+
+const product = 'Computer'
 console.log(product)
+
+const otherProduct = new String('Phone')
+console.log(otherProduct)
