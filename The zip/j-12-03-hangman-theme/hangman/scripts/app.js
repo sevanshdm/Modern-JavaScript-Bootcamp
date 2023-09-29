978bcf88698a568eb35cc074dc867b271b724d53
sevans <sevans@highdesertmilk.com>
2023-09-29 08:16:32 -0600

    VIdeo 122. Setting up the Hangman Theme

--- The zip/j-12-03-hangman-theme/hangman/scripts/app.js
+++ The zip/j-12-03-hangman-theme/hangman/scripts/app.js
@@ -1,15 +1,9 @@
-// HTTP (Hypertext Transfer Protocol)
-// Request - What do we want to do
-// Response - What was actually done
-
 const puzzleEl = document.querySelector('#puzzle')
 const guessesEl = document.querySelector('#guesses')
+let game1
 
-let game1 
-
-
-window.addEventListener('keydown', (event) => {
-    const guess = event.key
+window.addEventListener('keypress', (e) => {
+    const guess = String.fromCharCode(e.charCode)
     game1.makeGuess(guess)
     render()
 })
@@ -17,9 +11,8 @@ window.addEventListener('keydown', (event) => {
 const render = () => {
     puzzleEl.innerHTML = ''
     guessesEl.textContent = game1.statusMessage
-
-    //This manipulates what goes into the puzzle element
-    game1.puzzle.split('').forEach((letter)=> {
+    
+    game1.puzzle.split('').forEach((letter) => {
         const letterEl = document.createElement('span')
         letterEl.textContent = letter
         puzzleEl.appendChild(letterEl)
@@ -39,13 +32,11 @@ startGame()
 // getPuzzle('2').then((puzzle) => {
 //     console.log(puzzle)
 // }).catch((err) => {
-//     console.log(`Error ${err}`)
+//     console.log(`Error: ${err}`)
 // })
 
-
-                        // Success arg
 // getCurrentCountry().then((country) => {
-//     console.log(country.name.common)
+//     console.log(country.name)
 // }).catch((error) => {
 //     console.log(error)
 // })
