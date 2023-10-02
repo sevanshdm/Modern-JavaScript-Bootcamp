fd00e4a8d2ac5eea6b75c6637e6d6c1242b5a329
sevans <sevans@highdesertmilk.com>
2023-10-02 12:48:29 -0600

    VIdeo 144. Converting Notes App: Part 3

--- notes-app/src/index.js
+++ notes-app/src/index.js
@@ -1,5 +1,19 @@
-import { getNotes, createNote } from './notes.js'
+import { getNotes, createNote, removeNote, updateNote } from './notes.js'
+import { getFilters, setFilters} from './filters'
 
-console.log(getNotes())
-createNote()
-console.log(getNotes())
+// console.log(getNotes())
+// createNote()
+// removeNote('1')
+
+// updateNote('1',{
+//     title: 'My note title',
+//     body: 'This is the body for my note.'
+// })
+// console.log(getNotes())
+
+console.log(getFilters())
+setFilters({
+    searchText: 'Office',
+    sortBy: 'byCreated'
+})
+console.log(getFilters())
